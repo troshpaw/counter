b0e68a5980231c023ccfb04b177b36c376565cab
Pavel <troshpaw@gmail.com>
2024-07-17 11:41:53 +0300

    some corrections

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-import { data } from "./data/data.module.js";
+// import { data } from "./data/data.module.js";
 import { render } from "./render/render.module.js";
 import { setChangeDataCallback } from "./data/data.module.js";
 
